5b068546577fe9c2fa5861817a846d5daa177584
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-21 13:39:26 -0500

    Add full keyboard for standard printable characters to devcards

--- test/atreus/ui/base_test.cljs
+++ test/atreus/ui/base_test.cljs
@@ -5,10 +5,115 @@
             [reagent.core]))
 
 (defcard-rg key-labels
-  "All key labels are made with SVG sprites, and a little bit of magic."
+  "All key labels are made with AVG sprites, and a little bit of magic."
+
   [:div
-   [sut/label "q"]
-   [sut/label "w"]
-   [sut/label "e"]
-   [sut/label "r"]
-   ])
+   "Normal keyboard"
+   [:div
+    [sut/label "1"]
+    [sut/label "2"]
+    [sut/label "3"]
+    [sut/label "4"]
+    [sut/label "5"]
+    [sut/label "6"]
+    [sut/label "7"]
+    [sut/label "8"]
+    [sut/label "9"]
+    [sut/label "0"]
+    [sut/label "-"]
+    [sut/label "="]]
+
+   [:div
+    [sut/label "q"]
+    [sut/label "w"]
+    [sut/label "e"]
+    [sut/label "r"]
+    [sut/label "t"]
+    [sut/label "y"]
+    [sut/label "u"]
+    [sut/label "i"]
+    [sut/label "o"]
+    [sut/label "p"]
+    [sut/label "["]
+    [sut/label "]"]
+    [sut/label "\\"]]
+
+   [:div
+    [sut/label "a"]
+    [sut/label "s"]
+    [sut/label "d"]
+    [sut/label "f"]
+    [sut/label "g"]
+    [sut/label "h"]
+    [sut/label "j"]
+    [sut/label "k"]
+    [sut/label "l"]
+    [sut/label ";"]
+    [sut/label "'"]]
+
+   [:div
+    [sut/label "z"]
+    [sut/label "x"]
+    [sut/label "c"]
+    [sut/label "v"]
+    [sut/label "b"]
+    [sut/label "n"]
+    [sut/label "m"]
+    [sut/label ","]
+    [sut/label "."]
+    [sut/label "/"]]
+
+   "Shifted Keyboard"
+   [:div
+    [sut/label "!"]
+    [sut/label "@"]
+    [sut/label "#"]
+    [sut/label "$"]
+    [sut/label "%"]
+    [sut/label "^"]
+    [sut/label "&"]
+    [sut/label "*"]
+    [sut/label "("]
+    [sut/label ")"]
+    [sut/label "_"]
+    [sut/label "+"]]
+
+   [:div
+    [sut/label "q"]
+    [sut/label "w"]
+    [sut/label "e"]
+    [sut/label "r"]
+    [sut/label "t"]
+    [sut/label "y"]
+    [sut/label "u"]
+    [sut/label "i"]
+    [sut/label "o"]
+    [sut/label "p"]
+    [sut/label "{"]
+    [sut/label "}"]
+    [sut/label "|"]]
+
+   [:div
+    [sut/label "a"]
+    [sut/label "s"]
+    [sut/label "d"]
+    [sut/label "f"]
+    [sut/label "g"]
+    [sut/label "h"]
+    [sut/label "j"]
+    [sut/label "k"]
+    [sut/label "l"]
+    [sut/label ":"]
+    [sut/label "\""]]
+
+   [:div
+    [sut/label "z"]
+    [sut/label "x"]
+    [sut/label "c"]
+    [sut/label "v"]
+    [sut/label "b"]
+    [sut/label "n"]
+    [sut/label "m"]
+    [sut/label "<"]
+    [sut/label ">"]
+    [sut/label "?"]]])
