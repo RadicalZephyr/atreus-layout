7c9c1e29a535d0063b5103e87be861355fd6fe5f
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-21 11:09:42 -0500

    Add a favicon!!

--- src/atreus/layout.clj
+++ src/atreus/layout.clj
@@ -12,6 +12,12 @@
 (defn main-page []
   (page/html5
    [:head
+    [:link {:rel "apple-touch-icon" :sizes "180x180" :href "/apple-touch-icon.png"}]
+    [:link {:rel "icon" :type "image/png" :sizes "32x32" :href "/favicon-32x32.png"}]
+    [:link {:rel "icon" :type "image/png" :sizes "16x16" :href "/favicon-16x16.png"}]
+    [:link {:rel "manifest" :href "/manifest.json"}]
+    [:link {:rel "mask-icon" :href "/safari-pinned-tab.svg" :color "#5bbad5"}]
+    [:meta {:name "theme-color" :content "#ffffff"}]
     (page/include-css "/css/normalize.css"
                       "/css/app.css")]
    [:body
