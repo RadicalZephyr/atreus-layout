4bfb900d36bc994e6ffafd0e6ed1ea3754debec0
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-21 13:40:09 -0500

    Add lookup to label component for renaming xml special characters

--- src/atreus/ui/base.cljs
+++ src/atreus/ui/base.cljs
@@ -1,5 +1,12 @@
 (ns atreus.ui.base)
 
+(def char-names
+  {"<" "lt"
+   ">" "gt"
+   "'" "tick"
+   "\"" "quote"
+   "&" "amp"})
+
 (defn label [name]
   [:svg.label
-   [:use {"xlinkHref" (str "/img/key-sprites.svg#label_" name)}]])
+   [:use {"xlinkHref" (str "/img/key-sprites.svg#label_" (get char-names name name))}]])
