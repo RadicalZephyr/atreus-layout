70fd742279e83961e2fad7114a2ba27d0f20b131
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-22 11:49:27 -0500

    Add fn for reading all positions in a block of text

--- src/atreus/task/svg_fix.clj
+++ src/atreus/task/svg_fix.clj
@@ -5,3 +5,14 @@
 (defn adjust-all-positions [positions]
   (let [delta (mapv #(- % 20) (first positions))]
     (mapv #(mapv - % delta) positions)))
+
+(def pos-re #"d=\"m ([0-9.]+),([0-9.]+)")
+
+(defn read-int [s]
+  (Integer/parseInt s))
+
+(defn read-positions [text]
+  (->> (re-seq pos-re text)
+       (mapv #(->> %
+                   (drop 1)
+                   (mapv read-int)))))
--- test/atreus/task/svg_fix_test.clj
+++ test/atreus/task/svg_fix_test.clj
@@ -1,8 +1,9 @@
 (ns atreus.task.svg-fix-test
   (:require [atreus.task.svg-fix :as sut]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [clojure.string :as str]))
 
-(deftest test-things
+(deftest test-adjust-all-positions
   (is (= [[20 20]]
          (sut/adjust-all-positions [[150 200]])))
   (is (= [[20 20] [30 25]]
@@ -10,3 +11,16 @@
 
   (is (= [[20 20] [30 25] [40 40]]
          (sut/adjust-all-positions [[150 200] [160 205] [170 220]]))))
+
+(deftest test-read-positions
+  (is (= [[20 20]]
+         (sut/read-positions "d=\"m 20,20\"")))
+  (is (= [[30 20]]
+         (sut/read-positions "d=\"m 30,20\"")))
+  (is (= [[20 20] [30 20]]
+         (sut/read-positions (str/join "\n" ["d=\"m 20,20\""
+                                             "d=\"m 30,20\""]))))
+  (is (= [[20 20] [30 20]]
+         (sut/read-positions (str/join "\ngarbage and random stuff\n"
+                                       ["d=\"m 20,20\""
+                                        "d=\"m 30,20\""])))))
