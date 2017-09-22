2df8945925fb990744be270850d33a0181b5dc7a
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-22 14:20:14 -0500

    Use code to generate svg text elements instead of using sprites

--- src/atreus/ui/base.cljs
+++ src/atreus/ui/base.cljs
@@ -1,4 +1,5 @@
-(ns atreus.ui.base)
+(ns atreus.ui.base
+  (:require [clojure.string :as str]))
 
 (def char-names
   {"<" "lt"
@@ -7,6 +8,27 @@
    "\"" "quote"
    "&" "amp"})
 
+(defn- font-size-for [name]
+  (case (count name)
+    1 "40px"
+    2 "26px"
+    "15px"))
+
+(defn- coords-for [name]
+  (case (count name)
+    1 {:x "9" :y "30"}
+    2 {:x "8" :y "30"}
+    3 {:x "8" :y "24"}
+    {:x "9" :y "30"}))
+
 (defn label [name]
   [:svg.label
-   [:use {"xlinkHref" (str "/img/key-sprites.svg#label_" (get char-names name name))}]])
+   [:text {:style {:font-size (font-size-for name)
+                   :line-height "125%"
+                   :font-family "Anonymous Pro"
+                   :letter-spacing "0px"
+                   :fill "#000000"
+                   :fill-opacity "1"
+                   :stroke "none"}}
+    [:tspan (coords-for name)
+     (str/capitalize name)]]])
