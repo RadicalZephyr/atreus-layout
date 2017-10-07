31ee96f0dbd8fd0df4eb3dab4a24a396c97aebc1
Geoff Shannon <geoffpshannon@gmail.com>
2017-10-07 17:09:49 -0500

    Improve names in main cljs app

--- src/atreus/app.cljs
+++ src/atreus/app.cljs
@@ -2,19 +2,23 @@
   (:require [reagent.core :as reagent]
             [reagent.ratom :as ratom]
             [re-frame.core :as re-frame]
-            [atreus.ui.base :as base]))
+            [atreus.ui.base :as base]
+            [atreus.ui.modal :as modal]))
 
-(defn setup-re-frame! []
-  (re-frame/register-handler
+(defn setup! []
+  (re-frame/reg-event-db
    :initialise-db
    (fn
      [_ _]
-     {})))
+     {:modal-options {}})))
+
+(defn modal []
+  [base/fixed 20
+   [modal/modal-root "Hello Modal!" {:show true}]])
 
 (defn main-panel []
   [:div
-   [base/fixed 20
-    [base/modal-root "Hello Modal!" {:show true}]]
+   [modal]
    [:h1 "Hello World"]])
 
 (defn init-render! []
@@ -22,6 +26,6 @@
   (reagent/render [main-panel]
                   (.getElementById js/document "app")))
 
-(defn ^:export setup! []
-  (setup-re-frame!)
+(defn ^:export start! []
+  (setup!)
   (init-render!))
