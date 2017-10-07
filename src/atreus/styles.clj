1bbc021e2cef26f2aff52bd952c48527de3bee1c
Geoff Shannon <geoffpshannon@gmail.com>
2017-10-07 14:48:20 -0500

    Add some spacing to styles

--- src/atreus/styles.clj
+++ src/atreus/styles.clj
@@ -7,17 +7,24 @@
   [:* {:box-sizing "border-box"}]
 
   [:body {:font-family "Helvetica"}]
+
   [:#layout-root {:position "relative"
                   :z-index 1}]
+
   [:area {:cursor "pointer"}]
+
   [:.layout-label {:position "absolute"
                    :z-index -1}]
+
   [:svg.label {:width "40px"
                :height "40px"}]
+
   [:#com-rigsomelight-devcards-main
    [:svg.label {:border "solid black 1px"
                 :margin "5px"}]]
+
   [:.test {:background-color "tomato"}]
+
   (s/at-font-face
    {:font-family "Anonymous Pro"
     :src "url(\"/fonts/anonymous-pro.ttf\")"}))
